d157b8ccf339d63477ea72e5da93da4e6156ec8e
Mohammad <mohammad.s3dd@gmail.com>
2022-02-05 11:36:01 +0330

    init

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col">
+        <button @click="error(404)">404</button>
+        <button @click="error(100)">100</button>
+    </div>
+</template>
+<script setup>
+const error = (code) => {
+    const errorObj = new Error(`Error ${code}`)
+    errorObj.statusCode = code
+    const router = useNuxtApp().$router
+
+    router.replace({
+        path: '/oops/' + code,
+
+    })
+}
+</script>
---
+++ pages/returnError/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+definePageMeta({
+    middleware: 'redirect'
+})
+const nuxtApp = useNuxtApp()
+const error = (code) => {
+    const errorObj = new Error(`Error ${code}`)
+    const router = useNuxtApp().$router
+
+
+}
+const route = useRoute()
+const code = route.params.code
+error(code)
+</script>
